854bd8ffc70b6bf87df6ef0ec1d874bbbbbc0b47
DevlTz <kkvale27@gmail.com>
2025-07-03 11:43:36 -0300

    Add things

--- sorts_in_cpp/algorithm.cpp
+++ sorts_in_cpp/algorithm.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 
+
+
 int insertion_sort(int arr[], int lenght){
 // Start with the second element and compares if itself is smaller than the first.
 // Then, we going to compares the third element if is smaller then the others compared before.
 return 0;
+}
+
+// Big datas is not the best, but the smaller numbers or datas.
+
+
 }
 
 int selection_sort(int arr[], int lenght){
@@ -15,7 +22,14 @@ int selection_sort(int arr[], int lenght){
 // 2 > 4? X
 // 2 > 20 ? X ... 30
 // 2 5 > 4? V
-// 2 4 5 20 15 30... 
+// 2 4 5 20 15 30...
+
+
+
+
+
+//
+//
 return 0;
 }
 
@@ -35,13 +49,31 @@ void bubble_sort(int arr[], int lenght){
         if(!troca) break;
      }
 
+// Worst algorithm in the perfomance ?? Just to teach :) KKKKKKKKKKKKKK incrivel isso
+//
+// worst case o(n^2)
+// best case o(n)
+}
+
+void merge_sort(int arr[], int left, int right,){
+
+// o(n log n) -> n : number of items on array log n is the time of divide in the array.
+// omega(n log n)
+
+
+
+
+// Divide and conquer -> Parallel 
 
 }
 
-int merge_sort(int arr[], int lenght){return 0;}
-
 int quick_sort(int arr[], int lenght){
 return 0;
+
+// Pivot need to be on -> had to be choosed the element less or equal than pivot creating the new list
+//
+// worst o(n^2)
+// best o(n log n)
 }
 
 int main(){ 
@@ -59,6 +91,13 @@ bubble_sort(arr,lenght);
 std::cout << "Array ordenado pelo bubble sort" << std::endl;
 for (int i = 0; i < lenght; i++){
     std::cout << arr[i] << "\t";
+}
+std::cout << std::endl;
+
+ // merge_sort(arr, lenght);
+
+for (int i = 0; i < lenght; i++){
+    std::cout << arr[i] << "\t";    
 }
 std::cout << std::endl;
 
